b9b7e28953258e58ef17d1c77375b54094b94656
Dennis Katsonis <dennisk@netspace.net.au>
2018-04-22 11:19:32 +1000

    Added copyright notices to source files.

--- source/mailprotocol.d
+++ source/mailprotocol.d
@@ -1,3 +1,22 @@
+/*
+ * Spaminex: Mailbox utility to delete/bounce spam on server interactively.
+ * Copyright (C) 2018  Dennis Katsonis dennisk@netspace.net.au
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 // Super class for Mail protocol handling.
 import std.outbuffer;
 import std.typecons;
